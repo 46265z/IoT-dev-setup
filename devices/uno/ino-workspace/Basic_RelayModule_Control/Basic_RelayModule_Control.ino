fdc562650c9d455a29c96fea3f8a3e231b6c635a
Alexander Parvanov <47386361+YYx00xZZ@users.noreply.github.com>
2021-12-22 17:42:29 +0200

    Update Basic_RelayModule_Control.ino

--- devices/uno/ino-workspace/Basic_RelayModule_Control/Basic_RelayModule_Control.ino
+++ devices/uno/ino-workspace/Basic_RelayModule_Control/Basic_RelayModule_Control.ino
@@ -13,21 +13,22 @@ void setup() {
 }
 
 void loop() {
+  
   // Turn on the relay...
   Serial.println("Turning the relay on (digitalWrite)");
-  digitalWrite(RelayPin, LOW);
-  delay(400);
-  Serial.println("Reading pin state with digitalRead and returning the current state.");
-  delay(100);
-  Serial.println(digitalRead(RelayPin));
-  delay(2500);
+  digitalWrite(RelayPin, LOW);delay(400);
   
+  // Reading pin state with digitalRead and returning the current state.
+  Serial.println("current state");delay(100);
+  Serial.println(digitalRead(RelayPin));delay(2500);
+
+
+
   // Turn off the relay...
   Serial.println("Turning the relay off (digitalWrite)");
-  digitalWrite(RelayPin, HIGH);
-  delay(400);
-  Serial.println("Reading pin state with digitalRead and returning the current state.");
-  delay(100);
-  Serial.println(digitalRead(RelayPin));
-  delay(2500);
+  digitalWrite(RelayPin, HIGH);delay(400);
+  
+  // Reading pin state with digitalRead and returning the current state.
+  Serial.println("current state");delay(100);
+  Serial.println(digitalRead(RelayPin));delay(2500);
 }
